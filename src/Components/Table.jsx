f19ed0aa18c09aebe5bed0676229bbb31c26e9f3
romulo-rp29 <romulo.ceuma@gmail.com>
2022-05-09 16:31:37 -0300

    :sparkles:	feat: search by letter filter

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,13 +1,15 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PlanetsContext from '../Context/PlanetsContext';
 
 function Table() {
   const { tableHeads, info } = useContext(PlanetsContext);
 
+  const [search, setSearch] = useState('');
+
   const resultados = info.results;
 
   useEffect(() => {
-    console.log(resultados);
+    console.log(search);
   });
 
   return (
@@ -15,7 +17,8 @@ function Table() {
       <input
         type="text"
         data-testid="name-filter"
-        onChange={ () => {} }
+        onChange={ (event) => setSearch(event.target.value) }
+        value={ search }
       />
       <table>
         <thead>
@@ -26,25 +29,29 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {resultados && (
-            resultados.map((planet) => (
-              <tr key={ planet.name }>
-                <td data-testid="planet-name">{planet.name}</td>
-                <td>{planet.rotation_period}</td>
-                <td>{planet.orbital_period}</td>
-                <td>{planet.diameter}</td>
-                <td>{planet.climate}</td>
-                <td>{planet.gravity}</td>
-                <td>{planet.terrain}</td>
-                <td>{planet.surface_water}</td>
-                <td>{planet.population}</td>
-                <td>{planet.films}</td>
-                <td>{planet.created}</td>
-                <td>{planet.edited}</td>
-                <td>{planet.url}</td>
-              </tr>
-            ))
-          )}
+          {resultados
+           && (
+             resultados.map((planet) => (planet.name.toLowerCase()
+               .includes(search.toLowerCase()) ? (
+                 <tr key={ planet.name }>
+                   <td data-testid="planet-name">
+                     {planet.name}
+                   </td>
+                   <td>{planet.rotation_period}</td>
+                   <td>{planet.orbital_period}</td>
+                   <td>{planet.diameter}</td>
+                   <td>{planet.climate}</td>
+                   <td>{planet.gravity}</td>
+                   <td>{planet.terrain}</td>
+                   <td>{planet.surface_water}</td>
+                   <td>{planet.population}</td>
+                   <td>{planet.films}</td>
+                   <td>{planet.created}</td>
+                   <td>{planet.edited}</td>
+                   <td>{planet.url}</td>
+                 </tr>
+               ) : null))
+           )}
           {}
         </tbody>
       </table>
